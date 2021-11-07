27a3753479b486063449a5016565cbab986c73e9
Eddie <ed201112@gmail.com>
2021-11-06 21:03:03 -0700

    starting upload

--- src/components/VideoUpload/VideoUpload.jsx
+++ src/components/VideoUpload/VideoUpload.jsx
@@ -5,6 +5,18 @@ function VideoUpload(props) {
   return (
     <>
       <Nav />
+      <h1 className="upload__title">Upload Video</h1>
+      <div className="upload__block">
+        <div className="upload__subblock">
+          <div className="upload__subtitle">VIDEO THUMBNAIL</div>
+          <div className="upload__thumbnail"></div>
+        </div>
+        <div className="upload__subblock">
+          <form action="" method="post"></form>
+        </div>
+      </div>
+      
+
     </>
   );
 }
