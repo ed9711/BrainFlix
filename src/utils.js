11d285e0a6d0450fd872dc0b4f947ea08ce93447
Eddie <ed201112@gmail.com>
2021-11-11 19:47:34 -0800

    delete files

--- src/utils.js
+++
@@ -1,7 +0,0 @@
-export const timeStampToTime = (timeStamp) => {
-    const date = new Date(timeStamp);
-    const time = `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`;
-    return time;
-};
-
-export const apiKey = "?api_key=dec9fd5b-8dc0-43b7-9e72-219a359d9728";
